e7a64c307043f7eba317a7d7ec57b6ab65f15e00
unknown <ediasdoprado@gmail.com>
2017-11-16 20:48:40 -0200

    Commit inicial. Criação do projeto Dominio e das entidades.

---
+++ TesteFitcard/TesteFitcard.Dominio/Entidades/EntidadeBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TesteFitcard.Dominio.Entidades
+{
+    /// <summary>
+    /// Entidade que possui atributos comuns a diversas outras entidades.
+    /// </summary>
+    public class EntidadeBase
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+        public DateTime DataExclusao { get; set; }
+        public bool Excluido { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
